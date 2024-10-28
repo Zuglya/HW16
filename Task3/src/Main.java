f441b5bf2e2ced12eb5c2707a78fb7c983bc3ac8
Zuglya <irussel86@gmail.com>
2024-10-29 02:10:11 +0400

    Решение ДЗ Урока №16

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String VALID_REGEX = "([А-я-]+\\s)([А-я-]+\\s)([А-я-]+)";
+
     public static void main(String[] args) {
         //todo ФИО
+        Scanner scan = new Scanner(System.in);
+        String fio = scan.nextLine();
+
+        if(fio.matches(VALID_REGEX)) {
+            System.out.println(fio.replaceAll(VALID_REGEX, "Фамилия: $1\nИмя: $2\nОтчество: $3"));
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
     }
 }
--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static final String NUM_REGEX = "(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
+    public static final String NUM_FORMAT = "+7 ($2) $3 $4-$5";
+    public static final String SHORT_NUM_FORMAT = " +7 ($1) $2 $3-$4";
+
     public static void main(String[] args) {
         //todo Numbers
+//        String number = new Scanner(System.in).nextLine().trim();
+
+        String[] numberList = {"+7909123-45-67", "+7 (909) 1234567", "8-905-1234567", "9-453-1234567", "8-905-123", "905-1234567", "8-905-12345672342"};
+
+        for(String e: numberList) {
+            String number = e.replaceAll("\\D+", "");
+            if(number.length() == 11) {
+                number = number.replaceAll("(^\\d)" + NUM_REGEX, NUM_FORMAT);
+                System.out.println(number);
+            } else if(number.length() == 10) {
+                number = number.replaceAll(NUM_REGEX, SHORT_NUM_FORMAT);
+                System.out.println(number);
+            } else {
+                System.out.println(number + " - Это не номер");
+            }
+//            System.out.println(number);
+        }
+        System.out.println("");
     }
 }
